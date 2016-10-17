9c873776e03ef3e7b696fadc29b5d9fb0bb16614
Akan Nkweini <akan2000@gmail.com>
2016-10-17 11:24:54 +0100

    Calculate price of items and total price

--- src/test/java/om/capgemini/cafex/CustomerPurchasesTest.java
+++ src/test/java/om/capgemini/cafex/CustomerPurchasesTest.java
@@ -1,5 +1,6 @@
 package om.capgemini.cafex;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -10,15 +11,20 @@ import static org.junit.Assert.assertEquals;
  * @author akan
  */
 public class CustomerPurchasesTest {
+    private Assistant assistant;
+    private Customer customer;
+    private CustomerPurchases customerPurchases;
+    private CafeXMenu menu;
 
-    @Test
-    public void testAddItemToCustomerPurchase() throws Exception {
+    private static final double DELTA = 0.000001;
 
-        Assistant assistant = new Assistant();
-        Customer customer = new Customer();
-        CustomerPurchases customerPurchases = new CustomerPurchases(assistant, customer);
+    @Before
+    public void setUp() {
+        assistant = new Assistant();
+        customer = new Customer();
+        customerPurchases = new CustomerPurchases(assistant, customer);
 
-        CafeXMenu menu = new CafeXMenu(
+        menu = new CafeXMenu(
                 new HashMap<String, MenuItem>(){{
                     put("cola",           new MenuItem("Cola", "Cold Drink",           0.5));
                     put("coffee",         new MenuItem("Coffee", "Hot Drink",          1.0));
@@ -26,6 +32,10 @@ public class CustomerPurchasesTest {
                     put("steak sandwich", new MenuItem("Steak Sandwich", "Hot Food",   4.5));
                 }}
         );
+    }
+
+    @Test
+    public void testAddItemToCustomerPurchase() throws Exception {
 
         int noOfItemsBefore = customerPurchases.getNumberOfItems();
 
@@ -34,6 +44,36 @@ public class CustomerPurchasesTest {
         customerPurchases.addMenuItem(menu.getMenuItem("cola"));
 
         assertEquals(customerPurchases.getNumberOfItems(), noOfItemsBefore + 1);
+    }
 
+    @Test
+    public void testCustomerPurchaseItemPrices() {
+        addAllItems();
+
+        for (MenuItem item : customerPurchases.getItems()) {
+            switch (item.getName()) {
+                case "Cola":
+                    assertEquals(item.getPrice(), 0.5, DELTA);
+                    break;
+                case "Coffee":
+                    assertEquals(item.getPrice(), 1.0, DELTA);
+                    break;
+                case "Cheese Sandwich":
+                    assertEquals(item.getPrice(), 2.0, DELTA);
+                    break;
+                case "Steak Sandwich":
+                    assertEquals(item.getPrice(), 4.5, DELTA);
+                    break;
+            }
+        }
+
+        assertEquals(customerPurchases.getTotalPrice(), 8.0, DELTA);
+    }
+
+    private void addAllItems() {
+        customerPurchases.addMenuItem(menu.getMenuItem("cola"));
+        customerPurchases.addMenuItem(menu.getMenuItem("coffee"));
+        customerPurchases.addMenuItem(menu.getMenuItem("chess sandwich"));
+        customerPurchases.addMenuItem(menu.getMenuItem("steak sandwich"));
     }
 }
